61510202148b9034ed2cb352c941e6ba7902d543
Felipe Freitas <soufreitas.felipe@gmail.com>
2023-11-06 15:15:42 -0300

    Finalização das Tags e seus botões

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,9 +1,62 @@
 
+import styled from 'styled-components';
+import tags from './tags.json';
 
-const Tags =()=>{
 
-    return(
-        <p>Tags</p>
+const TesteFlex = styled.div`
+    display:flex;
+    align-items:center;
+    gap:10px;    
+`
+
+const TituloTag = styled.h2`
+    font-size:24px;
+    color:#D9D9D9;
+    font-weight:400;
+    /* margin:0; */
+`
+
+const BotaoEstilizado = styled.button`
+
+    font-size:24px;
+    color:#FFFFFF;
+    border-radius: 10px;
+   
+    background: rgba(217, 217, 217, 0.3);
+    cursor:pointer;
+
+    border: 2px solid transparent;
+
+    box-sizing:border-box;
+    transition: background-color 0.3s ease;
+    &:hover{
+        border-color:#C98CF1;
+    }
+
+`
+
+const DivBotao = styled.div`
+    display:flex;
+    gap:24px;
+    margin-left:24px;
+`
+
+const Tags = () => {
+
+    return (
+        <TesteFlex>
+
+
+            <TituloTag>Busque por Tags:</TituloTag>
+
+        <DivBotao>
+
+            {tags.map(tag => <BotaoEstilizado key={tag.id}>{tag.titulo}</BotaoEstilizado>)
+            }
+            </DivBotao>
+        </TesteFlex>
+
+
     )
 }
 
